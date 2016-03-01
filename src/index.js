d7636f31f05fbbf5e49749564a95a9a53f1ae889
Colten Rouska <crouska@gmail.com>
2016-03-01 08:36:25 -0700

    Intitial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const Hapi = require('hapi');
+const Good = require('good');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: function (request, reply) {
+    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+  }
+});
+
+server.register({
+  register: Good,
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      events: {
+        response: '*',
+        log: '*'
+      }
+    }]
+  }
+}, (err) => {
+
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start((err) => {
+    if (err) {
+     throw err;
+    }
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+});
